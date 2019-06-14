ce7505d3ec8267e04541a918c720c4b91ac21abc
Damian Wojtowicz <C17413722@mydit.ie>
2019-06-14 13:58:03 +0100

    Headless Chrome

--- PythonFiles/code.py
+++ PythonFiles/code.py
@@ -7,6 +7,35 @@ from random import randrange
 #from win32con import *
 from numpy import *
 #import pyperclip
+#import imgkit
+#import pdfkit
+#import wkhtmltopdf
+#from wkhtmltopdf import WKhtmlToPdf
+
+import click
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import shutil
+
+chromePath = "C:\\Users\\User1\\Downloads\\OCALL\\chromedriver.exe"
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument('--headless')
+chromeOptions.add_argument(f'window-size={1920}x{1080}')
+driver = 0
+
+
+"""
+    - Get comments
+    - Put comments into an array and then into a dictionary, key being the parent ID
+    - Pull comment out one by one
+    - With the comment that has been obtained split it into an array of string at every comma or full stop
+    - Take the HTML file and input all the info (username, points, time etc)
+    - Put the first line of the comment into it
+    - Get screenshot of the html file
+    - Put in the next line
+    - Get next screenshot and repeat the process
+"""
 
 import praw
 reddit = praw.Reddit(client_id = 'W7V-Goda74pQFA',
@@ -20,7 +49,7 @@ hot_python= subreddit.hot(limit = 1)
 
 commentDict = {}
 
-def getComments():
+def getComments(amount):
     for submission in hot_python:
         if not submission.stickied:
             print(submission.title, "\n")
@@ -31,8 +60,8 @@ def getComments():
             for comment in submission.comments.list():
                 # comment is the first comment
                 if comment.parent() == submission:
-                    if commentCount == 0:
-                        print(dir(comment))
+                    #if commentCount == 0:
+                        #print(dir(comment))
                     
                     # key exists
                     if comment.parent() in commentDict:
@@ -42,7 +71,7 @@ def getComments():
                         commentDict[comment.parent()] = [comment]
 
                     commentCount += 1
-                    if commentCount > 10:
+                    if commentCount >= amount:
                         break
 
 def printComments():
@@ -53,13 +82,27 @@ def printComments():
             #print('Comment ID:', comment.id)
             print(comment.body)
 
-def getStylesheet():
-    
+def startDriver():
+    global driver
+    driver = webdriver.Chrome(executable_path = chromePath, options = chromeOptions)
+
+def captureHTMl(srcNum):
+    driver.get("http://localhost//TalkReddit//Comments.html")
+    driver.execute_script("document.body.style.zoom='250%'")
+    driver.save_screenshot("attempt"+ srcNum +".png")
+    driver.quit()
+
+def copyFile():
+    shutil.copy2('C://xampp//htdocs//TalkReddit//Comments_Base.html', 'C://xampp//htdocs//TalkReddit//Comments.html')
+ 
 
 # runs at the start
 def main():
-    getComments()
+    getComments(1)
     printComments()
+    startDriver()
+    copyFile()
+    captureHTMl()
  
 if __name__ == '__main__':
     main()
