adb730d7b0d0fd79deabda45ce7e8a7f33004a08
Damian Wojtowicz <C17413722@mydit.ie>
2019-06-18 10:43:59 +0100

    Transferr to PC

--- PythonFiles/code.py
+++ PythonFiles/code.py
@@ -13,6 +13,7 @@ from numpy import *
 #import wkhtmltopdf
 #from wkhtmltopdf import WKhtmlToPdf
 #import ffmpeg
+import re
 
 import click
 from selenium import webdriver
@@ -52,13 +53,15 @@ reddit = praw.Reddit(client_id = 'W7V-Goda74pQFA',
                      user_agent = 'AzureScale1')
 
 subreddit = reddit.subreddit('askreddit')
-submission1 = reddit.submission(url='https://www.reddit.com/r/AskReddit/comments/c01upz/you_can_fill_a_pool_with_anything_you_want_money/')
+submission2 = reddit.submission(url='https://www.reddit.com/r/AskReddit/comments/c01upz/you_can_fill_a_pool_with_anything_you_want_money/')
+submission1 = reddit.submission(url='https://www.reddit.com/r/AskReddit/comments/c193hp/whats_the_most_disturbing_secret_youve_been_told/')
 #hot_python = subreddit.hot(limit = 1)
 hot_python = [submission1]
 
 commentDict = {}
 
 repoPath = 'C:\\Users\\User1\\Desktop\\Talk Reddit\\VideoMakerRepo\\'
+thisFilePath = repoPath + "PythonFiles"
 
 def getComments(amount):
     for submission in hot_python:
@@ -91,6 +94,7 @@ def printComments():
             print(20*'-')
             #print('Parent ID:', comment.parent())
             #print('Comment ID:', comment.id)
+            #cleanString(comment.body)
             print(comment.body)
 
 def startDriver():
@@ -106,6 +110,9 @@ def captureHTMl(srcNum, threadID, commentID):
 
 def copyFile():
     shutil.copy2('C://xampp//htdocs//TalkReddit//Comments_Base.html', 'C://xampp//htdocs//TalkReddit//Comments.html')
+
+def cleanString(s):
+    pass
 
 def splitComments():
     comments = []
@@ -123,6 +130,7 @@ def splitComments():
                 endIndex += 1
                 if ( endIndex == commBodyLen or char == '.' and commentBody[endIndex] != '.' or char == ',' and not commentBody[endIndex - 2].isdigit() or char == '?'):
                     sentence = commentBody[sIndex:endIndex]
+                    sentence = cleanString(sentence)
                     sIndex = endIndex
                     sentences.append(sentence)
                     #print(sentence)
@@ -136,7 +144,8 @@ def splitComment(commentBody):
     sentences = []
     sIndex = 0
     endIndex = 0
-    print(commentBody)
+    #print(commentBody)
+    commentBody = commentBody.replace('\n', '<br>')
     commBodyLen = len(commentBody)
     
     for char in commentBody:
@@ -149,13 +158,10 @@ def splitComment(commentBody):
     return sentences
 
 def replaceText(newText):
-    #driver.execute_script("""document.querySelector("username_here");""")
-    #inputElement = driver.find_element_by_id('commentBodyText')
     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "commentBodyText")))
-    newText = element.text + newText
-    #print("newText:", newText)
-    #driver.execute_script("arguments[0].innerText = \'" +"\\"+ str(newText)+"\\"+ "\'", element)
-    driver.execute_script("arguments[0].textContent = arguments[1];", element, newText)
+    #newText = element.text + newText
+    #print(newText)
+    driver.execute_script("arguments[0].innerHTML = arguments[1];", element, newText)
 
 def clearText():
     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "commentBodyText")))
@@ -182,24 +188,39 @@ def createDir(threadID, CommentID):
     if not os.path.isdir(path):
         print("comment path doesn't exist")
         os.mkdir(path)
+    # if a folder for a comment already exists then remove the comment folder and create a new one under the same comment ID
     else:
-        print("deleting path")
-        
-        #os.mkdir(path)
+        shutil.rmtree(path)
+        os.mkdir(path)
 
-def deleteThread(threadID)
+def deleteThread(threadID):
     path = "../Videos/" + threadID
     shutil.rmtree(path)
 
-def makeVideo():
-    index = 4
-    #print( os.getcwd() )
-    #os.chdir('C://Users/User1/Desktop/Talk Reddit/VideoMakerRepo/PythonFiles/')
-    subprocess.call('ffmpeg -loop 1 -i imageFile.png -i audioFile.wav -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest out6.mp4', shell=True)
+def makeVideo(threadID, commentID):
+    #index = 4
+    print("Making video")
+    path = repoPath + 'Videos\\' + threadID + "\\" + commentID
+    os.chdir(path)
+    for i in range (1, 8):
+        subprocess.call('ffmpeg -loop 1 -framerate 30 -i ' + str(i) + '.png -i ' + str(i) + '.wav -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest outt' + str(i) + '.mp4', shell=True)
+
+    #subprocess.call('ffmpeg -f concat -i soundOrder.txt -c copy outputX.wav', shell=True)
+    os.chdir(thisFilePath)
+    print("Making video ended")
+
+def checkLineBreak(s):
+    if "\n" in s:
+        print("~~~~~~~~~YES~~~~~~~~")
+    """
+    for i in s:
+        if i == "\"
+            print("New Line Found: ")
+    """
     
 # runs at the start
 def main():
-    getComments(2)
+    getComments(8)
     startDriver()
     copyFile()
     commentIndex = 0
@@ -207,11 +228,13 @@ def main():
     global submission1
     for key in commentDict.keys():
         for comment in commentDict[key]:
+            htmlText = ""
             imageCounter = 1
             threadID = str(submission1)
             commentID = str(comment)
             createDir(threadID, commentID)
-            comment = splitComment(comment.body)
+            commentBody = comment.body
+            comment = splitComment(commentBody)
 
             commentLen = len(comment)
             index = 1
@@ -220,6 +243,7 @@ def main():
             clearText()
             for commentPiece in comment:
                 # write to file
+                #writeToFile(str(commentIndex), commentBody, threadID, commentID)
                 writeToFile(str(commentIndex), commentPiece, threadID, commentID)
                 
                 # if second last piece 
@@ -227,12 +251,19 @@ def main():
                     divVis("commentFooter", "visible")
                     
                 # add text
-                replaceText(commentPiece)
+                htmlText = htmlText + commentPiece
+                replaceText(htmlText)
+                #replaceText(commentBody)
+                #print(20*'-')
+                #checkLineBreak(commentBody)
+                #print(repr(commentBody))
                 time.sleep(0.5)
                 # take screenshot and save it
                 captureHTMl(str(imageCounter), threadID, commentID)
                 index += 1
                 imageCounter += 1
+
+            #makeVideo(threadID, commentID)
 
     """
     comments = splitComments()
@@ -273,11 +304,16 @@ def testVoices():
 
     #engine.say('Hello World')
     #engine.runAndWait()
+
+def testText():
+    getComments(15)
+    printComments()
      
 if __name__ == '__main__':
     #writeToFile("1", "String")
-    #makeVideo()
-    #main()
-    deleteThread(threadID)
+    #makeVideo("c01upz", "er0sc11")
+    main()
+    #testText()
+    #deleteThread("c01upz")
     #testVoices()
 
